5dc6b39bd780605c19529a4c6625b7e35c56a108
Cesar Brasil <cesaraugustokg6@hotmail.com>
2018-08-21 13:26:08 -0300

    dafsdf

---
+++ cloud-functions-angular-start/src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD-3se_J6DRDPAIMmJr1GbfejO8uhXtMLc',
+    authDomain: 'chat-angular-43a1c.firebaseapp.com',
+    databaseURL: 'https://chat-angular-43a1c.firebaseio.com',
+    storageBucket: 'chat-angular-43a1c.appspot.com',
+    messagingSenderId: '984683732337'
+  }
+};
